18024e708caf6f7aa0132354b7e276fbe676528a
J2ghz <j2.00ghz@gmail.com>
2017-11-02 11:00:09 +0100

    Receive and Execute, Tests

--- NNTP Client Tests/ConnectionTests.cs
+++ NNTP Client Tests/ConnectionTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NNTP_Client;
 
 namespace NNTP_Client_Tests
@@ -10,9 +9,25 @@ namespace NNTP_Client_Tests
         [TestMethod]
         public void ConnectTest()
         {
-            var connection = new Connection("news.dotsrc.org",119);
+            var connection = new Connection("news.dotsrc.org", 119);
             Assert.IsNotNull(connection);
             Assert.IsNotNull(connection.ns);
+        }
+
+        [TestMethod]
+        public void ConnectReadMessage()
+        {
+            var connection = new Connection("news.dotsrc.org", 119);
+            var message = connection.WelcomeMessage;
+            Assert.AreEqual("200 news.sunsite.dk NNRP Service Ready - staff@sunsite.dk (posting ok).", message);
+        }
+
+        [TestMethod]
+        public void GetHelpMessage()
+        {
+            var connection = new Connection("news.dotsrc.org", 119);
+            var result = connection.Execute("help");
+            Assert.IsTrue(result.StartsWith("100"), $"result should start with 100\n{result}");
         }
     }
 }
--- NNTP Client/Connection.cs
+++ NNTP Client/Connection.cs
@@ -1,15 +1,35 @@
-﻿using System.Net.Sockets;
+﻿using System.IO;
+using System.Net.Sockets;
 
 namespace NNTP_Client
 {
     public class Connection
     {
+        private readonly StreamReader reader;
+        private readonly StreamWriter writer;
+
         public Connection(string hostname, int port)
         {
             var tcp = new TcpClient(hostname, port);
             ns = tcp.GetStream();
+            reader = new StreamReader(ns);
+            writer = new StreamWriter(ns);
+            WelcomeMessage = Receive();
         }
 
+        public string WelcomeMessage { get; }
         public NetworkStream ns { get; }
+
+        public string Receive()
+        {
+            return reader.ReadLine();
+        }
+
+        public string Execute(string command)
+        {
+            writer.WriteLine(command);
+            writer.Flush();
+            return Receive();
+        }
     }
 }
