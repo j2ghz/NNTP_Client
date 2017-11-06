248761abcc0348080932afdf093e26660a818228
J2ghz <j2.00ghz@gmail.com>
2017-11-06 09:17:22 +0100

    Add group model with parsing

---
+++ NNTP Client Tests/Models/GroupTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NNTP_Client.Models;
+
+namespace NNTP_Client_Tests.Models
+{
+    [TestClass]
+    public class GroupTests
+    {
+        [TestMethod]
+        [DataRow("alt.1d 0000001252 000000459 y", "alt.1d", 1252UL, 459UL, true)]
+        [DataRow("alt.0d 0000001257 000000364 y", "alt.0d", 1257UL, 364UL, true)]
+        [DataRow("alt.2600 0000024553 000001183 y", "alt.2600", 24553UL, 1183UL, true)]
+        public void Parse(string groupstring,string name, ulong first,ulong last,bool available)
+        {
+            var group = new Group(groupstring);
+            Assert.AreEqual(name,group.Name);
+            Assert.AreEqual(first, group.FirstArticle);
+            Assert.AreEqual(last, group.LastArticle);
+            Assert.AreEqual(available, group.ArticleAvailable);
+        }
+
+
+    }
+}
---
+++ NNTP Client/Models/Group.cs
@@ -0,0 +1,19 @@
+﻿namespace NNTP_Client.Models
+{
+    public class Group
+    {
+        public Group(string data)
+        {
+            var dataArr = data.Split(' ');
+            Name = dataArr[0];
+            FirstArticle =ulong.Parse(dataArr[1]);
+            LastArticle = ulong.Parse(dataArr[2]);
+            ArticleAvailable =  dataArr[3] == "y";
+        }
+
+        public string Name { get; private set; }
+        public ulong FirstArticle { get; private set; }
+        public ulong LastArticle { get; private set; }
+        public bool ArticleAvailable { get; private set; }
+    }
+}
